34eab44b27e9f4c01278b1bf1d84f1ccbb1ff59b
Chen yg <83961207+Yanguangchen@users.noreply.github.com>
2024-09-20 16:06:41 +0800

    Refactor CSS and add Medium Publication Feed iframe

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ root.render(
       {/* <DarkMode /> */}
       <NavBar />
       <FloatingSideButton />
+      <iframe
+        src="https://widgets.sociablekit.com/medium-publication-feed/iframe/25465384"
+        title="Medium Publication Feed"
+        style={{border: "none", overflow: "hidden", frameborder: "0", height: "250vh", width: "100%" }}
+      ></iframe>
     </div>
   </>
 );
